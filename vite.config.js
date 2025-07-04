065591eebbcdf55a4cc0524a65a5ef180a4c36a6
Ashim Choudhary <panghalashim7832@gmail.com>
2025-07-04 19:50:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || '/gta-6-landing-page',
   plugins: [react(), tailwindcss()],
 });
